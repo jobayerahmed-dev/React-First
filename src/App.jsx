758b35700ef87b09c0725408a1f7b4f0d0f8e799
jobayerahmed-dev <jobayerahmed735@gmail.com>
2025-10-26 17:02:04 +0600

    working useState in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-import Avater from "./componnent/Avatar";
+import { useState } from "react";
+export default function Form() {
+  const [firstName, setFirstName] = useState(0);
+  const [lastName, setLastName] = useState(0);
 
-export default function App() {
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  function handleReset() {
+    setFirstName("");
+    setLastName("");
+  }
+
   return (
-    <Avater parsion={{ name: "jobayer ahmed", imageId: "szV5sdG" }} size={50} />
+    <form onSubmit={(e) => e.preventDefault()}>
+      <input
+        placeholder="First name"
+        value={firstName}
+        onChange={handleFirstNameChange}
+      />
+      <input
+        placeholder="Last name"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
+      <h1>
+        Hi, {firstName} {lastName}
+      </h1>
+      <button onClick={handleReset}>Reset</button>
+    </form>
   );
 }
